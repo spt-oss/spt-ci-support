f3ef326bc910d2337924931eed2e1c1eb18bb8a4
SPT Developer <spt.developer@gmail.com>
2018-01-16 11:50:30 +0900

    fix mvn

--- command/mvn-go-offline.sh
+++ command/mvn-go-offline.sh
@@ -8,24 +8,14 @@ function self::command() {
 	# TODO https://issues.apache.org/jira/browse/MDEP-516
 	# TODO https://issues.apache.org/jira/browse/MDEP-568
 	#mvn dependency:go-offline
+	
+	# Install plugins, dependencies and artifacts
 	mvn install --batch-mode -DskipTests -Dcheckstyle.skip=true
 	
+	# Install plugins after install
 	mvn \
-		help:help \
-		clean:help \
-		source:help \
 		javadoc:help \
-		resources:help \
-		minify:help \
-		compiler:help \
-		surefire:help \
-		jar:help \
 		spring-boot:help \
-		checkstyle:help \
-		gpg:help \
-		license:help \
-		install:help \
-		release:help \
 		--batch-mode
 }
 
